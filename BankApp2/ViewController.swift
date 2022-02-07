30e2985138d14943dbdbcdca471eccab072cbe8a
Danil Ryumin <ryumin_danil@icloud.com>
2022-02-07 21:20:43 +0300

    Initial Commit

---
+++ BankApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp2
+//
+//  Created by Danil Ryumin on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
